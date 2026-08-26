acaf10038c9a0c565714a09ea794ca66c1aabc8e
phunlh2001 <phunlh2001@gmail.com>
2026-08-27 00:41:26 +0700

    chore: update base for config

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -36,6 +36,7 @@ export default defineConfig({
   },
   renderer: {
     root: ".",
+    base: "./",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
