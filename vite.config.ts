9a75484ff1772406e03ad28da201aded25152ab1
phunlh2001 <phunlh2001@gmail.com>
2026-08-26 23:57:41 +0700

    fixed: added base path to config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
